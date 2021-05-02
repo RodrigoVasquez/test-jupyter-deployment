4c1edd75eef95bb903dc75c5f5db77c65832e532
Rodrigo Vasquez <rodrigo.vasquez.fernandez@gmail.com>
2021-05-02 17:14:33 -0400

    removing version

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,6 +1,5 @@
 module "subnets" {
     source  = "terraform-google-modules/network/google//modules/subnets"
-    version = "~> 2.0.0"
 
     project_id   = var.project_id
     network_name = var.network
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -15,7 +15,6 @@
 
 module "vpc" {
   source  = "terraform-google-modules/network/google//modules/vpc"
-  version = "~> 2.0.0"
   
   project_id   = "${var.project}"
   network_name = "${var.env}"
