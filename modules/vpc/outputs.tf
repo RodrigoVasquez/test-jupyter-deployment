c15d51e411e144a9cc15bd665f784e7b68c2070c
Rodrigo Vasquez <rodrigo.vasquez.fernandez@gmail.com>
2021-05-02 17:17:47 -0400

    removing ouputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -12,14 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-output "subnet" {
-  value = "${element(module.vpc.subnets, 0)}"
-}
-
-output "subnet_gke" {
-  value = "${element(module.vpc.subnets, 1)}"
-}
-
 output "network" {
   value = "${module.vpc.network}"
 }
